a4a8618d6d97555ea97ca4b42b63e792106c395b
Anastasiya Yakovleva <dovakot@yandex.ru>
2021-06-08 20:21:28 +0800

    Подключает библиотеку classnames

--- project/src/components/logo/logo.jsx
+++ project/src/components/logo/logo.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-
-const setClass = (flag) => `logo__link ${flag ? 'logo__link--light' : ''}`;
+import cn from 'classnames';
 
 function Logo({isModifier}) {
+  const logoClass = cn('logo__link', {'logo__link--light': isModifier});
+
   return (
     <div className="logo">
-      <a className={setClass(isModifier)} href="/">
+      <a className={logoClass} href="/">
         <span className="logo__letter logo__letter--1">W</span>
         <span className="logo__letter logo__letter--2">T</span>
         <span className="logo__letter logo__letter--3">W</span>
