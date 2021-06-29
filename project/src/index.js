93a21aab7e6524bcc740bcc558df9196ff2f681a
Anastasiya Yakovleva <dovakot@yandex.ru>
2021-06-29 22:13:05 +0800

    Добавит модуль для моковых данных

--- project/src/index.js
+++ project/src/index.js
@@ -3,15 +3,15 @@ import ReactDOM from 'react-dom';
 import App from './components/app/app';
 
 import {
-  PROMO_FILM,
-  FILMS
-} from './mock';
+  filmData,
+  promoFilmData
+} from './mocks/films';
 
 ReactDOM.render(
   <React.StrictMode>
     <App
-      promoFilm={PROMO_FILM}
-      films={FILMS}
+      films={filmData}
+      promoFilm={promoFilmData}
     />
   </React.StrictMode>,
   document.getElementById('root'));
