bf627e2018e8b9e1527383323c7020c5f5bc0f11
Anastasiya Yakovleva <dovakot@yandex.ru>
2021-06-29 22:40:47 +0800

    Свяжет страницы приложения

--- project/src/components/pages/not-found/not-found.jsx
+++ project/src/components/pages/not-found/not-found.jsx
@@ -1,23 +1,20 @@
 import React from 'react';
-import {
-  Link
-} from 'react-router-dom';
+import {Link} from 'react-router-dom';
 
+import {AppRoute} from '../../../const';
 import PageTitle from '../../ui/page-title/page-title';
 
 function NotFound() {
   return (
     <div className="user-page">
       <header className="page-header user-page__head">
-        <PageTitle
-          title="404"
-        />
+        <PageTitle title="404" />
       </header>
 
       <p>
         К сожалению, запрашиваемая страница не найдена. Возможно, вы перешли
         по ссылке, в которой была допущена ошибка, или ресурс был удален.
-        Попробуйте начать с <Link to="/">главной страницы</Link>.
+        Попробуйте начать с <Link to={AppRoute.ROOT}>главной страницы</Link>.
       </p>
     </div>
   );
--- project/src/components/ui/logo/logo.jsx
+++ project/src/components/ui/logo/logo.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import {
-  Link
-} from 'react-router-dom';
-
+import {Link} from 'react-router-dom';
 import cn from 'classnames';
 
+import {AppRoute} from '../../../const';
+
 function Logo({isModifier}) {
-  const logoClass = cn('logo__link', {'logo__link--light': isModifier});
+  const logoClass = cn('logo__link', {
+    'logo__link--light': isModifier,
+  });
 
   return (
     <div className="logo">
-      <Link className={logoClass} to="/">
+      <Link className={logoClass} to={AppRoute.ROOT}>
         <span className="logo__letter logo__letter--1">W</span>
         <span className="logo__letter logo__letter--2">T</span>
         <span className="logo__letter logo__letter--3">W</span>
