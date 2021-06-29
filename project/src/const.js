e6a46f1064fb8e3a27c2bc9bfad4f2a61a17a2df
Anastasiya Yakovleva <dovakot@yandex.ru>
2021-06-29 22:51:08 +0800

    Создаст перечисление

--- project/src/const.js
+++ project/src/const.js
@@ -1,12 +1,10 @@
-import PropTypes from 'prop-types';
-
 const AppRoute = {
   ROOT: '/',
   LOGIN: '/login',
   FILM: '/films/:id',
   REVIEW: '/films/:id/review',
   MY_LIST: '/mylist',
-  PLAYER: '/player',
+  PLAYER: '/player/:id',
 };
 
 const PosterModifier = {
@@ -14,7 +12,43 @@ const PosterModifier = {
   SMALL: 'small',
 };
 
+const GenreInfo = {
+  DEFAULT: 'All genres',
+  COUNT: 9,
+};
+
+const FilmInfo = {
+  MAX_FILM_COUNT: 4,
+  MAX_ACTOR_COUNT: 4,
+  MAX_STAR_COUNT: 10,
+  PLAY_DELAY_TIME: 1000,
+  RATINGS: [
+    {
+      rating: 10,
+      title: 'Awesome',
+    },
+    {
+      rating: 8,
+      title: 'Very good',
+    },
+    {
+      rating: 5,
+      title: 'Good',
+    },
+    {
+      rating: 3,
+      title: 'Normal',
+    },
+    {
+      rating: 0,
+      title: 'Bad',
+    },
+  ],
+};
+
 export {
   AppRoute,
-  PosterModifier
+  PosterModifier,
+  GenreInfo,
+  FilmInfo
 };
